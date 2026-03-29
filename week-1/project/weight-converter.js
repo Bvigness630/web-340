8d532ebf0d9f3bf48953f0c4c68336312d61b18e
Boyd Vigness <vignessboyd@gmail.com>
2026-03-29 13:41:57 -0500

    Add weight-converter

---
+++ week-1/project/weight-converter.js
@@ -0,0 +1,33 @@
+/**
+ * Author: Boyd Vigness
+ * Date: 3/29/2026
+ * File Name: weight-converter.js
+ * Description:
+*/
+
+"use strict";
+
+// TODO: Implement the weight conversion logic here
+
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.error("Usage: node weight-converter.js <pounds>");
+  process.exit(1);
+}
+
+const pounds = args[0];
+
+if (isNaN(pounds)) {
+  console.error("Input must be a number.");
+  process.exit(1);
+}
+
+// Convert to number
+const poundsNum = Number(pounds);
+
+// Convert pounds to kilograms
+const kilograms = poundsNum * 0.453592;
+
+// Output result rounded to 2 decimal places
+console.log(kilograms.toFixed(2));
